1420f316df7093ac8f7a79b60aee39ad9de79713
KyleH <22343261@studentmail.ul.ie>
2025-04-07 15:10:12 +0100

    Beginning of RestaurantView and file Structure

---
+++ app/src/main/java/com/example/unieats/controller/RestaurantController.java
@@ -0,0 +1,18 @@
+package com.example.unieats.controller;
+
+import com.example.unieats.R;
+import com.example.unieats.model.Restaurant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantController {
+
+    public List<Restaurant> getRestaurants() {
+        List<Restaurant> list = new ArrayList<>();
+        list.add(new Restaurant("Tasty Bites", "Delicious homemade food", 4.5, R.drawable.scholars_club));
+        list.add(new Restaurant("Ocean View", "Seafood with a view", 4.7, R.drawable.scholars_club));
+//        list.add(new Restaurant("Pizza Palace", "Wood-fired pizza", 4.3, R.drawable.restaurant3));
+        return list;
+    }
+}
--- app/src/main/java/com/example/unieats/controller/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.example.unieats.controller;
-
-public class UserController {
-}
