c677c1589ab64bf3c39bddea83dd630e34dd4710
JayeeshK04 <22083421@studentmail.ul.ie>
2025-04-30 18:08:44 +0100

    Added about us page and functionality

---
+++ app/src/main/java/com/example/unieats/view/activity/AboutUsActivity.java
@@ -0,0 +1,26 @@
+package com.example.unieats.view.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.unieats.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
