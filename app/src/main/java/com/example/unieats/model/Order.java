a30285993e2b945abaddb569ab9802c4f4af3ffb
LM-22347526 <22347526@studentmail.ul.ie>
2025-05-01 17:01:43 +0100

    Added more order stuff

--- app/src/main/java/com/example/unieats/model/Order.java
+++ app/src/main/java/com/example/unieats/model/Order.java
@@ -9,17 +9,28 @@ public class Order {
     private List<Menu.MenuItem> menuItems;
     private boolean pending;
     private int orderNumber;
+    private Timestamp orderTime;
+    private Timestamp collectionTime;
+    private String businessName;
 
     public Order() {
         this.menuItems = new ArrayList<>();
         this.pending = false;
         this.orderNumber = 0;
+        this.orderTime = Timestamp.now();
+        this.businessName = "";
     }
 
     public Order(List<Menu.MenuItem> menuItems, boolean pending) {
         this.menuItems = menuItems;
         this.pending = pending;
         this.orderNumber = ((int) Timestamp.now().getSeconds());
+        this.orderTime = Timestamp.now();
+        this.collectionTime = new Timestamp(
+                orderTime.getSeconds() + ((15 * 60) + (int) (Math.random() * 15)),
+                orderTime.getNanoseconds()
+        );
+        this.businessName = menuItems.get(0).getBusinessName();
     }
 
 
@@ -46,5 +57,29 @@ public class Order {
 
     public void setOrderNumber(int orderNumber) {
         this.orderNumber = orderNumber;
+    }
+
+    public Timestamp getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Timestamp orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    public Timestamp getCollectionTime() {
+        return collectionTime;
+    }
+
+    public void setCollectionTime(Timestamp collectionTime) {
+        this.collectionTime = collectionTime;
+    }
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
     }
 }
