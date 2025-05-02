e6c1f914c81b52c35e4f13baf21a097b7a03a08a
LM-22347526 <22347526@studentmail.ul.ie>
2025-05-02 14:37:21 +0100

    Added writeOrderHistory() and readOrderHistory()

--- app/src/main/java/com/example/unieats/controller/OrderController.java
+++ app/src/main/java/com/example/unieats/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.example.unieats.controller;
 
+import android.content.Context;
+
 import com.example.unieats.model.Menu;
 import com.example.unieats.model.Order;
 import com.google.firebase.database.DataSnapshot;
@@ -8,6 +10,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.Query;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,6 +23,8 @@ import java.util.Objects;
 
 public class
 OrderController {
+    private static final String orderHistoryFileName = "order_history.dsv";
+
     public interface OrderCallback {
         void onOrdersLoaded(List<Order> orders);
     }
@@ -105,6 +114,33 @@ OrderController {
         });
     }
 
+    public static void getCompletedOrders(OrderCallback callback) {
+        FirebaseDatabase db = FirebaseDatabase.getInstance("https://unieats-57c3e-default-rtdb.europe-west1.firebasedatabase.app/");
+
+        DatabaseReference dbRef = db.getReference("Order/data");
+
+        Query query = dbRef.orderByChild("pending").equalTo(false);
+
+        query.get().addOnCompleteListener(task -> {
+            List<Order> result = new ArrayList<>();
+
+            if (task.isSuccessful()) {
+                GenericTypeIndicator<HashMap<String, Order>> typeIndicator = new GenericTypeIndicator<HashMap<String, Order>>() {
+                };
+
+                HashMap<String, Order> firebaseResult = task.getResult().getValue(typeIndicator);
+
+                if (firebaseResult != null) {
+                    for (Map.Entry<String, Order> e : firebaseResult.entrySet()) {
+                        result.add(e.getValue());
+                    }
+                }
+
+                callback.onOrdersLoaded(result);
+            }
+        });
+    }
+
     public static void updateOrderStatus(String orderNumber, boolean isPending) {
         FirebaseDatabase db = FirebaseDatabase.getInstance("https://unieats-57c3e-default-rtdb.europe-west1.firebasedatabase.app/");
         DatabaseReference dbRef = db.getReference("Order/data");
@@ -123,5 +159,30 @@ OrderController {
                 }
             }
         });
+    }
+
+    public static void writeOrderHistory(Context context, int orderNumber) {
+        ArrayList<Integer> currentHistory = readOrderHistory(context);
+    }
+
+    private static ArrayList<Integer> readOrderHistory(Context context) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        try (FileInputStream fis = context.openFileInput(orderHistoryFileName)) {
+            InputStreamReader inputStreamReader = new InputStreamReader(fis, StandardCharsets.UTF_8);
+
+            try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
+                String line = reader.readLine();
+
+                while(line != null) {
+                    result.add(Integer.parseInt(line));
+                    line = reader.readLine();
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
     }
 }
